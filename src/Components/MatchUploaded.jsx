453621499d7983d3fef4768f4544c98f5ea40c11
Piyush Khatri <piyushkhatri968@gmail.com>
2025-05-24 00:59:54 +0500

    .

--- src/Components/MatchUploaded.jsx
+++ src/Components/MatchUploaded.jsx
@@ -95,7 +95,7 @@ const MatchUploaded = () => {
 
       {/* Modal */}
       {showForm && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+        <div className="fixed inset-0 flex justify-center items-center z-50 bg-black/70">
           <div className="bg-white rounded-lg p-6 w-full max-w-md relative">
             <button
               className="absolute top-3 right-3 text-gray-600 hover:text-gray-900 text-2xl font-bold"
