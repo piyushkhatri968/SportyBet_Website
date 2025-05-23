cf6c7baff7bda5e478ed4786b4a684580ef86042
Piyush Khatri <piyushkhatri968@gmail.com>
2025-05-23 12:53:49 +0500

    .

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-// export const backend_URL = "https://betapp-api-jklb.vercel.app/api";
-// export const backend_URL1 = "https://betapp-api-jklb.vercel.app";
-export const backend_URL = "http://localhost:5002/api";
-export const backend_URL1 = "http://localhost:5002";
+export const backend_URL = "https://betapp-api-jklb.vercel.app/api";
+export const backend_URL1 = "https://betapp-api-jklb.vercel.app";
+// export const backend_URL = "http://localhost:5002/api";
+// export const backend_URL1 = "http://localhost:5002";
