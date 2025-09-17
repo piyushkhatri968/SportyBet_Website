eedf806e9e262e4b5bdbd2416780aa7cb83c8d48
Piyush Khatri <piyush.khatri@benchmatrix.ca>
2025-09-17 09:31:57 +0500

    loading animation updated

--- src/Components/PageLoader.jsx
+++ src/Components/PageLoader.jsx
@@ -1,18 +1,19 @@
-// import logo from "../../assets/logo.png";
+import logo from "../assets/logo.webp";
+import "./PageLoader.css"; // import custom CSS
 
 const PageLoader = () => {
   return (
     <div className="min-h-screen w-full flex items-center justify-center bg-white">
       <div className="relative flex items-center justify-center">
         {/* Spinning Circle Outline */}
-        <div className="absolute w-50 h-50 border-6 border-[#A6ADBB] border-t-transparent rounded-full animate-[spin_2s_linear_infinite]"></div>
+        <div className="loader-circle"></div>
 
-        {/* Logo */}
+        {/* Logo with pulse */}
         <img
-          src="https://s3.us-east-1.amazonaws.com/cdn.designcrowd.com/blog/120-cool-logos-for-a-fresh-new-look/gaming-battle-soldier-sword-by-amcstudio-brandcrowd.png"
+          src={logo}
           alt="logo"
           draggable="false"
-          className="w-40 select-none"
+          className="loader-logo"
         />
       </div>
     </div>
